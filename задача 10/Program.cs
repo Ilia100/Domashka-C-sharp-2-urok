faf8293f0ad79f978d57d983280de5854907ee63
ilia <ilia100@hotmail.com>
2023-01-23 18:23:07 +0300

    задача 10 решена

--- задача 10/Program.cs
+++ задача 10/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int secondNumber(int x)
+{
+{
+return (x / 10)  - (x / 100)*10;
+}
+}
+
+Console.WriteLine($"введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number >99 && number <1000 || number <-99 && number >-1000)
+{
+int num = secondNumber(number);
+Console.WriteLine("второе число это " + num );
+
+}
+else
+{
+    Console.WriteLine($"число не трехзначное");
+}
